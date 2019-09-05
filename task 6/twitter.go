88a0725ffcd64815297829e545fbe8e463ec3583
Akshay Praveen Nair <52925407+iammarco11@users.noreply.github.com>
2019-09-05 11:00:23 +0530

    Update twitter.go

--- task 6/twitter.go
+++ task 6/twitter.go
@@ -1,23 +1,64 @@
 package main
 
+
 import (
-	"github.com/dghubble/oauth1"
-	"github.com/dghubble/oauth1/twitter"
-	"bufio"
-    "fmt"
-    "net/http"
-	"github.com/coreos/pkg/flagutil"
-	
-	
-		
+    
+    
+    "github.com/dghubble/go-twitter/twitter"
+    "github.com/dghubble/oauth1"
+    
+    
+	"log"
+	"os"
+	"fmt"
 )
 
-func main() {
-	config := oauth1.NewConfig("consumerKey", "consumerSecret")
-	token := oauth1.NewToken("accessToken", "accessSecret")
-	
-	httpClient := config.Client(oauth1.NoContext, token)
+type Credentials struct {
+    ConsumerKey       string
+    ConsumerSecret    string
+    AccessToken       string
+    AccessTokenSecret string
+}
+func getClient(creds *Credentials) (*twitter.Client, error) {
+    
+    config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
+    
+    token := oauth1.NewToken(creds.AccessToken, creds.AccessTokenSecret)
 
-	
-	client := twitter.NewClient(httpClient)	
+    httpClient := config.Client(oauth1.NoContext, token)
+    
+    client := twitter.NewClient(httpClient)
+
+    
+    verifyParams := &twitter.AccountVerifyParams{
+        SkipStatus:   twitter.Bool(true),
+        IncludeEmail: twitter.Bool(true),
+    }
+      
+    user, _, err := client.Accounts.VerifyCredentials(verifyParams)
+    if err != nil {
+        return nil, err
+    }
+
+    log.Printf("User's ACCOUNT:\n%+v\n", user)
+    return client, nil
+}
+func main() {
+    
+    creds := Credentials{
+        AccessToken:       os.Getenv("1168486308882173952-wL37ykXc8O700Uh0Bv3BbxgRmrYfX9"),
+        AccessTokenSecret: os.Getenv("T4DwOuFtzfQSxGRlKyldthmKtHWgXcKmXvuh8GVtoNwf9"),
+        ConsumerKey:       os.Getenv("Tts8r6QfmHku8UFWLm8G1BCfA"),
+        ConsumerSecret:    os.Getenv("4qz2TS5Y5HmLWKaeGln6rjWoOCBMhc0wx9TdHWjO2PQixKVUb9"),
+    }
+
+    client, err := getClient(&creds)
+    if err != nil {
+        log.Println("Error getting Twitter Client")
+        log.Println(err)
+    }
+
+    
+    fmt.Printf("%+v\n", client)
+
 }
