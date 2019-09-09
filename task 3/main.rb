af259c8cc295e3416e0236efb9071d2fb170e53e
Akshay Praveen Nair <52925407+iammarco11@users.noreply.github.com>
2019-09-09 19:42:15 +0530

    Update main.rb

--- task 3/main.rb
+++ task 3/main.rb
@@ -1,5 +1,7 @@
 require 'nokogiri'
-=> true
-irb(main):003:0> require 'open-uri'
-=> true
-irb(main):004:0> doc = Nokogiri::HTML(open('https://www.google.com/search?client=ubuntu&channel=fs&q=linux&ie=utf-8&oe=utf-8'))
+require 'open-uri'
+result = Nokogiri::HTML(open('https://www.google.com/search?q=linux'))
+result.css('div').each do |link|
+puts link.text
+puts ""
+end
