9139f008bcccd389a811e97f3d31fd4af7e8aabb
Akshay Praveen Nair <52925407+iammarco11@users.noreply.github.com>
2019-09-15 19:33:00 +0530

    Update main.rb

--- task 3/main.rb
+++ task 3/main.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 result = Nokogiri::HTML(open('https://www.google.com/search?start=1&end=10&q=linux’))
-result.css('a').each do |link|
+result.css('h3.LC20lb div.ellip').each do |link|
 puts link.text
 puts ""
 end
